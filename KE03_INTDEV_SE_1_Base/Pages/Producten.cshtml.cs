784656a295132ce3a9ea6548273d534531c30573
Divancia-ICT <2403974ananda@zuyd.nl>
2025-05-23 11:59:14 +0200

    ik heb voor elkaar gekregen dat de producten automatisch naar de winkelwagen gaan en dat de totaal aantal bedrag ook meegroeid

--- KE03_INTDEV_SE_1_Base/Pages/Producten.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Producten.cshtml.cs
@@ -1,41 +1,80 @@
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc.RazorPages;
+//using Bestellingplaatsen.Helpers;
+
+
+//namespace Bestellingplaatsen.Pages
+//{
+//    public class ProductenModel : PageModel
+//    {
+//        public void OnGet() { }
+
+//        public IActionResult OnPostAddToCart(int productId)
+//        {
+//            var product = GetProductById(productId);
+
+//            List<Product> cart = HttpContext.Session.GetObject<List<Product>>("Cart") ?? new List<Product>();
+//            cart.Add(product);
+//            HttpContext.Session.SetObject("Cart", cart);
+
+//            return RedirectToPage("/Winkelwagen");
+//        }
+
+//        private Product GetProductById(int id)
+//        {
+//            var producten = new List<Product>
+//            {
+//                new Product { Id = 1, Name = "Nebuchadnezzar", Price = 10000.0 },
+//                new Product { Id = 2, Name = "Jack-in Chair", Price = 500.5 },
+//                new Product { Id = 3, Name = "EMP (Electro-Magnetic Pulse) Device", Price = 129.99 }
+//            };
+//            return producten.FirstOrDefault(p => p.Id == id);
+//        }
+
+//        public class Product
+//        {
+//            public int Id { get; set; }
+//            public string Name { get; set; }
+//            public double Price { get; set; }
+//        }
+//    }
+//}
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using Bestellingplaatsen.Helpers;
-
 
 namespace Bestellingplaatsen.Pages
 {
     public class ProductenModel : PageModel
     {
-        public void OnGet() { }
-
-        public IActionResult OnPostAddToCart(int productId)
+        // Productenlijst (optioneel, want jij hebt ze hardcoded in de cshtml)
+        private readonly List<Product> _producten = new()
         {
-            var product = GetProductById(productId);
+            new Product { Id = 1, Naam = "Nebuchadnezzar", Prijs = 10000.00m },
+            new Product { Id = 2, Naam = "Jack-in Chair", Prijs = 500.50m },
+            new Product { Id = 3, Naam = "EMP (Electro-Magnetic Pulse) Device", Prijs = 129.99m }
+        };
 
-            List<Product> cart = HttpContext.Session.GetObject<List<Product>>("Cart") ?? new List<Product>();
-            cart.Add(product);
-            HttpContext.Session.SetObject("Cart", cart);
+        public List<Product> Producten => _producten;
 
-            return RedirectToPage("/Winkelwagen");
-        }
-
-        private Product GetProductById(int id)
+        // Hier voeg je je OnPost toe:
+        public IActionResult OnPost(int productId)
         {
-            var producten = new List<Product>
-            {
-                new Product { Id = 1, Name = "Nebuchadnezzar", Price = 10000.0 },
-                new Product { Id = 2, Name = "Jack-in Chair", Price = 500.5 },
-                new Product { Id = 3, Name = "EMP (Electro-Magnetic Pulse) Device", Price = 129.99 }
-            };
-            return producten.FirstOrDefault(p => p.Id == id);
-        }
+            var winkelwagen = HttpContext.Session.GetObject<List<int>>("Winkelwagen") ?? new List<int>();
+            winkelwagen.Add(productId);
+            HttpContext.Session.SetObject("Winkelwagen", winkelwagen);
 
-        public class Product
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public double Price { get; set; }
+            // Redirect naar winkelwagen of naar producten pagina
+            return RedirectToPage("Winkelwagen");
         }
+    }
+
+    public class Producten
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal PRiJS { get; set; }
     }
 }
--- KE03_INTDEV_SE_1_Base/Pages/Winkelwagen.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/Winkelwagen.cshtml.cs
@@ -1,14 +1,53 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Bestellingplaatsen.Helpers;
-
+using System.Collections.Generic;
+using System.Linq;
+using Bestellingplaatsen.Helpers; // Zorg dat je deze namespace toevoegt!
 
 namespace Bestellingplaatsen.Pages
 {
     public class WinkelwagenModel : PageModel
     {
+        private readonly List<Product> _producten = new()
+        {
+            new Product { Id = 1, Naam = "Nebuchadnezzar", Prijs = 10000.00m },
+            new Product { Id = 2, Naam = "Jack-in Chair", Prijs = 500.50m },
+            new Product { Id = 3, Naam = "EMP (Electro-Magnetic Pulse) Device", Prijs = 129.99m }
+        };
+
+        public List<WinkelwagenItem> Items { get; set; } = new();
+        public decimal TotaalPrijs { get; set; }
+
         public void OnGet()
         {
+            var winkelwagen = HttpContext.Session.GetObject<List<int>>("Winkelwagen") ?? new List<int>();
+
+            Items = winkelwagen
+                .GroupBy(id => id)
+                .Select(g =>
+                {
+                    var product = _producten.FirstOrDefault(p => p.Id == g.Key);
+                    return new WinkelwagenItem
+                    {
+                        Product = product,
+                        Aantal = g.Count()
+                    };
+                })
+                .ToList();
+
+            TotaalPrijs = Items.Sum(i => i.Product.Prijs * i.Aantal);
         }
     }
-}
 
+    public class WinkelwagenItem
+    {
+        public Product Product { get; set; }
+        public int Aantal { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public decimal Prijs { get; set; }
+    }
+}
